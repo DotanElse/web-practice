3720758197097b04fd1b325a94bfd6655dadb8eb
DotanElse <dotanelse@gmail.com>
2022-11-13 12:48:18 +0200

    Lousy RockPaperScissors game

--- javascript.js
+++ javascript.js
@@ -23,3 +23,44 @@ function FizzBuzz(max)
         console.log(result);
     }
 }
+
+RockPaperScissors()
+
+function RockPaperScissors()
+{
+    const optionsArr= ["rock", "paper", "scissors"]
+    let playerPoints = 0
+    let computerPoints = 0
+    for(let i=0; i<5; i++)
+    {
+        let playerChoice
+        do
+        {
+            playerChoice = prompt("Please enter your choice (rock, paper, scissors):")
+            playerChoice = playerChoice.toLowerCase()
+        }while(!optionsArr.includes(playerChoice))
+        computerChoice = optionsArr[Math.floor(Math.random() * optionsArr.length)]
+        console.log("computerChoice is" + computerChoice)
+        if(playerChoice != computerChoice)
+        {
+            computerChoiceVal = optionsArr.indexOf(computerChoice)
+            playerChoiceVal = optionsArr.indexOf(playerChoice)
+            if((computerChoiceVal+1)%3==playerChoiceVal)
+            {
+                playerPoints+=1
+                alert("Player win this round")
+            }
+
+            else
+            {
+                computerPoints+=1
+                alert("Computer win this round")
+            }
+        }
+    }
+    if(playerPoints > computerPoints)
+        alert("Wohoo player wins i guess")
+    else
+        alert("Bohoo player loses i guess")
+
+}
